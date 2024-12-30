afc540487bab4e2ddd9ad7c9fd44a9d448586ef7
Charm Thiekshana Perera <Charmthiekshana97@outlook.com>
2024-12-31 02:30:42 +0530

    update chemicals

--- src/api/chemical/controllers/chemical.ts
+++ src/api/chemical/controllers/chemical.ts
@@ -1,7 +1,51 @@
+'use strict';
+
 /**
- * chemical controller
+ * chemicals controller
  */
 
-import { factories } from '@strapi/strapi'
+const { createCoreController } = require('@strapi/strapi').factories;
 
-export default factories.createCoreController('api::chemical.chemical');
+module.exports = createCoreController('api::chemical.chemical', ({ strapi }) => ({
+  // Custom controller logic (if any) can go here
+  async find(ctx) {
+    const { query } = ctx;
+    const entities = await strapi.entityService.findMany('api::chemical.chemical', {
+      filters: query.filters,
+      sort: query.sort,
+      populate: query.populate,
+    });
+    return entities;
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.entityService.findOne('api::chemical.chemical', id, {
+      populate: ctx.query.populate,
+    });
+    return entity;
+  },
+
+  async create(ctx) {
+    const { body } = ctx.request;
+    const entity = await strapi.entityService.create('api::chemical.chemical', {
+      data: body,
+    });
+    return entity;
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+    const { body } = ctx.request;
+    const entity = await strapi.entityService.update('api::chemical.chemical', id, {
+      data: body,
+    });
+    return entity;
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+    await strapi.entityService.delete('api::chemical.chemical', id);
+    ctx.status = 204;
+  },
+}));
--- src/api/chemical/routes/chemical.ts
+++ src/api/chemical/routes/chemical.ts
@@ -1,7 +1,51 @@
-/**
- * chemical router
- */
+'use strict';
 
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::chemical.chemical');
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/chemicals',
+      handler: 'chemical.find',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'GET',
+      path: '/chemicals/:id',
+      handler: 'chemical.findOne',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/chemicals',
+      handler: 'chemical.create',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'PUT',
+      path: '/chemicals/:id',
+      handler: 'chemical.update',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/chemicals/:id',
+      handler: 'chemical.delete',
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
