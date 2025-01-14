66406e24871a4a4da0401c2b0bbda4392e69aa33
Charm Thiekshana Perera <Charmthiekshana97@outlook.com>
2025-01-14 08:11:28 +0530

    tool update

---
+++ src/api/utilize/controllers/utilize.ts
@@ -0,0 +1,7 @@
+/**
+ * utilize controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::utilize.utilize');
---
+++ src/api/utilize/services/utilize.ts
@@ -0,0 +1,7 @@
+/**
+ * utilize service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::utilize.utilize');
